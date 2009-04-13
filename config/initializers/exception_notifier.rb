f4528e31ba4a0a132e6e0bac3017ca8742421776
root <root@agiledisciple.(none)>
2009-04-13 23:51:03 +0000

    Removed rpm again, configured sending mail

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,1 +1,3 @@
 ExceptionNotifier.exception_recipients = [Enki::Config.default[:author, :email]]
+ExceptionNotifier.sender_address = Enki::Config.default[:author, :email]
+ExceptionNotifier.email_prefix = "[AgileDisciple] "
